237b6f9c125e23f451faa66f050a396be4ec6971
Matthew Kenny <matthewkenny@gmail.com>
2013-11-21 21:55:08 -0800

    Added usage notes.

--- pgsql2gist.py
+++ pgsql2gist.py
@@ -1,6 +1,40 @@
 __author__ = 'matt'
 __date__ = '11/19/13'
 __version__ = '0.1'
+"""
+POST GeoJSON or TopoJSON features from PostGIS to a Github Gist.
+
+Example usage: python pgsql2gist.py --host localhost --user matt tilestache \
+               "SELECT name, ST_AsGeoJSON(geom) AS geometry FROM neighborhoods LIMIT 5;"
+
+Current SELECT Statement Requirements:
+ - Geometry must be in EPSG:4326 WGS84 coordinate system
+ - Geometry must be wrapped in ST_AsGeoJSON(), ST_AsTopoJSON()
+
+usage: pgsql2gist.py [-f FILE] [-d DESCRIPTION] [-h HOST] [-p PORT]
+                     [-P PASSWORD] [-u USER] [-g GEOM_COL] [-?]
+                     database SELECT
+
+positional arguments:
+  database              PostGIS database name.
+  SELECT                SELECT Statement. NOTE: Geometry must be GeoJSON
+                        stored in WGS84.
+
+optional arguments:
+  -f FILE, --file FILE  Filename. NOTE: Must end in 'geosjon' or 'topojson'
+                        extension. (default: upload.geojson)
+  -d DESCRIPTION, --description DESCRIPTION
+                        Description of upload (default: File uploaded using
+                        pgsql2gist.)
+  -h HOST, --host HOST  PostGIS database hostname. (default: localhost)
+  -p PORT, --port PORT  PostGIS database port. (default: 5432)
+  -P PASSWORD, --password PASSWORD
+                        PostGIS user password. (default: None)
+  -u USER, --user USER  PostGIS database user. (default: postgres)
+  -g GEOM_COL, --geom-col GEOM_COL
+                        Geometry Column. (default: geometry)
+  -?, --help
+"""
 
 from psycopg2.extras import RealDictCursor
 from psycopg2 import connect, Error
