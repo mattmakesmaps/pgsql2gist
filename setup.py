ef4b5edb246401abd80d7ae20b0236fc95fbcf53
Matthew Kenny <matthewkenny@gmail.com>
2014-01-18 14:18:44 -0800

    bump to version 0.3.0

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ readme = open(os.path.join(here, 'README.rst')).read()
 
 setup(
     name='pgsql2gist',
-    version='0.2.2',
+    version='0.3.0',
     author='Matthew Kenny',
     author_email='matthewkenny AT gmail DOT com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Topic :: Scientific/Engineering :: GIS',
         'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
         'Intended Audience :: Developers'
@@ -22,7 +22,7 @@ setup(
     url='https://github.com/mattmakesmaps/pgsql2gist',
     license='GNU LESSER GENERAL PUBLIC LICENSE',
     keywords='gis gists postgis postgresql geojson',
-    description='like pgsql2shp, but for github gists',
+    description='Create maps using the Github Gist API as GeoJSON or TopoJSON, directly from PostGIS.',
     long_description=readme,
     install_requires=requires,
     test_suite='nose.collector',
